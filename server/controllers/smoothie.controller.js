cb314ab8d8db52943333f5b141ac687080bf6d76
Benton-Michael <mbenton002@gmail.com>
2022-06-24 13:02:48 -0600

    Michael/ added a second route to test pull/push

--- server/controllers/smoothie.controller.js
+++ server/controllers/smoothie.controller.js
@@ -1,72 +1,66 @@
-const Smoothie = require("../models/smoothies.model")
+const Smoothie = require("../models/smoothies.model");
 
 const createNewSmoothie = (req, res) => {
-    // CRUD - Create
-    Smoothie.create(req.body)
+  // CRUD - Create
+  Smoothie.create(req.body)
     .then((newSmoothie) => {
-        res.json({ newSmoothie });
+      res.json({ newSmoothie });
     })
     .catch((err) => {
-        res.status(400).json({err});
+      res.status(400).json({ err });
     });
 };
-    // Read
-//     const getAllSmoothies = (req, res) => {
-//         Smoothie.find()
-//         .then((allSmoothies) => {
-//             res.json({ allSmoothies });
-//         })
-//         .catch((err) => {
-//             res.status(400).json({ err });
-//         });
-// };
+// Read
+const getAllSmoothies = (req, res) => {
+  Smoothie.find()
+    .then((allSmoothies) => {
+      res.json({ allSmoothies });
+    })
+    .catch((err) => {
+      res.status(400).json({ err });
+    });
+};
 
-    // Get One  
-//     const getOneSmoothie = (req, res) => {
-//         Smoothie.findOne({ _id: req.params.id })
-//         .then((queriedSmoothie) => {
-//             res.json({ queriedSmoothie });
-//         })
-//         .catch((err) => {
-//             res.status(400).json({ err });
-//         });
-// };
+// Get One
+const getOneSmoothie = (req, res) => {
+  Smoothie.findOne({ _id: req.params.id })
+    .then((queriedSmoothie) => {
+      res.json({ queriedSmoothie });
+    })
+    .catch((err) => {
+      res.status(400).json({ err });
+    });
+};
 
+// Update
+const updateSmoothie = (req, res) => {
+  Smoothie.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+    runValidators: true,
+  })
+    .then((updatedSmoothie) => {
+      res.json({ updatedSmoothie });
+    })
+    .catch((err) => {
+      res.status(400).json({ err });
+    });
+};
 
+// Delete
+const deleteSmoothie = (req, res) => {
+  Smoothie.deleteOne({ _id: req.params.id })
+    .then((deletedResponse) => {
+      res.json({ deletedResponse });
+    })
+    .catch((err) => {
+      res.status(400).json({ err });
+    });
+};
 
-    // Update
-//     const updateSmoothie = (req, res) => {
-//         Smoothie.findOneAndUpdate({ _id: req.params.id }, req.body, {
-//             new: true,
-//             runValidators: true,
-//         })
-//         .then((updatedSmoothie) => {
-//             res.json({ updatedSmoothie });
-//         })
-//         .catch((err) => {
-//             res.status(400).json({ err });
-//         });
-// };
-
-
-
-    // Delete
-//     const deleteSmoothie = (req, res) => {
-//         Smoothie.deleteOne({ _id: req.params.id })
-//         .then((deletedResponse) => {
-//             res.json({ deletedResponse });
-//         })
-//         .catch((err) => {
-//             res.status(400).json({ err });
-//         });
-// };
-
-
-
-module.exports = { 
-    createNewSmoothie,
-    // getAllSmoothies,
-    // getOneSmoothie,
-    // updateSmoothie,
-    // deleteSmoothie,
- };
+module.exports = {
+  createNewSmoothie,
+  getAllSmoothies,
+  getOneSmoothie,
+  updateSmoothie,
+  deleteSmoothie,
+};
