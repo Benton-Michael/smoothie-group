0396e29e67bc89d35faddc6fdddc6e3deafa8e72
Benton-Michael <mbenton002@gmail.com>
2022-06-26 18:35:42 -0600

    michael test after re-cloning

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,15 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Welcome = () => {
-    return (
-        <div>Welcome to SipSpace
-            <h2>Sip Sip Sip</h2>
-            <h3>Just confirming I can push changes again!-Michael</h3>
-
-
-        </div>
-        
-    );
+  return (
+    <div>
+      Welcome to SipSpace
+      <h2>Sip Sip Sip</h2>
+      <h3>Just confirming I can push changes again!-Michael</h3>
+    </div>
+  );
 };
 
 export default Welcome;
