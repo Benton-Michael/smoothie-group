71772773562595dc17d62974a560a7dd361e80ee
Benton-Michael <mbenton002@gmail.com>
2022-06-26 18:26:12 -0600

    Michael test Push after cloning the repo again

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const Welcome = () => {
     return (
-        <div>Welcome to SipSpace</div>
+        <div>Welcome to SipSpace
+            <h2>Sip Sip Sip</h2>
+            <h3>Just confirming I can push changes again!-Michael</h3>
+
+
+        </div>
+        
     );
 };
 
