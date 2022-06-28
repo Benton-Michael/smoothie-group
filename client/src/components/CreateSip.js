c1f40972b5d3f79fbbb08dfa06ac47c6a2277159
Jason Walworth <jrwalworth@gmail.com>
2022-06-27 21:41:43 -0700

    fixing merge bugs with createsip and account route.

--- client/src/components/CreateSip.js
+++ client/src/components/CreateSip.js
@@ -17,47 +17,6 @@ const CreateSip = (props) => {
   const navigate = useNavigate();
 
 
-    return (
-        <>
-            <h3>Create your Favorite Smoothie</h3>
-            <form onSubmit={submitHandler} className="form SipForm">
-                <section className='input-area d-flex align-items-center'>
-                    <label  className='form-label col-3' htmlFor="method">Method: </label>
-                    <select name="method" id="method" value={method} className="form-control" onChange={(e) => setMethod(e.target.value)}>
-                        <option value="Pick-up">Pick-up</option>
-                        <option value="Delivery">Delivery</option>
-                    </select>
-                </section>
-                <section className='input-area d-flex align-items-center'>
-                    <label className="col-3" htmlFor="size">Size: </label>
-                    <select name="size" id="size" value={size} className="form-control" onChange={(e) => setSize(e.target.value)}>
-                        <option value="Small">Small</option>
-                        <option value="Medium">Medium</option>
-                        <option value="Large">Large</option>
-                        <option value="SuperSip">SuperSip</option>
-                    </select>
-                </section>
-                <section className='input-area d-flex align-items-center'>
-                    <label className="col-3" htmlFor="quantity">Qty: </label>
-                    <input type="Number" className="form-control" value={quantity} name='quantity' onChange={(e) => setQuantity(e.target.value)}/>
-                </section>
-                <section className='input-area d-flex align-items-center'>
-                    <label  className='form-label col-3' htmlFor="liquid">Liduid (Base): </label>
-                    <select name="liquid" id="liquid" value={liquid} className="form-control" onChange={(e) => setLiquid(e.target.value)}>
-                        <option value="Fruit Juice">Fruit Juice</option>
-                        <option value="Soy Milk">Soy Milk</option>
-                        <option value="Milk">Milk</option>
-                        <option value="Oat Milk">Oat Milk</option>
-                        <option value="Yogurt">Yogurt</option>
-                        <option value="Ice-Cream">Ice-Cream</option>
-                    </select>
-                    
-                </section>
-
-                
-                <input type="submit" value='Create' className="btn btn-primary mt-3" />
-            </form>
-
   const submitHandler = (e) => {
     e.preventDefault();
     axios
