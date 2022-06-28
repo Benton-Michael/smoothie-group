508df492d54afb02dadc2548e9cced21f786c70b
Joel Latta <joellatta24@gmail.com>
2022-06-28 15:42:50 -0700

    login bug fix

--- server/controllers/smoothie.controller.js
+++ server/controllers/smoothie.controller.js
@@ -22,48 +22,6 @@ const getAllSmoothies = (req, res) => {
       res.status(400).json({ message: "Error in getAllSmoothies", error: err });
     });
 };
-<<<<<<< Updated upstream
-=======
-    // Read
-    const getAllSmoothies = (req, res) => {
-        Smoothie.find()
-        .then((allSmoothies) => {
-            res.json({ allSmoothies });
-        })
-        .catch((err) => {
-            res.status(400).json({ err });
-        });
-// };
-
-    // Get One  
-    const getOneSmoothie = (req, res) => {
-        Smoothie.findOne({ _id: req.params.id })
-        .then((queriedSmoothie) => {
-            res.json({ queriedSmoothie });
-        })
-        .catch((err) => {
-            res.status(400).json({ err });
-        });
-// };
-
-
-
-    // Update
-//     const updateSmoothie = (req, res) => {
-//         Smoothie.findOneAndUpdate({ _id: req.params.id }, req.body, {
-//             new: true,
-//             runValidators: true,
-//         })
-//         .then((updatedSmoothie) => {
-//             res.json({ updatedSmoothie });
-//         })
-//         .catch((err) => {
-//             res.status(400).json({ err });
-//         });
-// };
-
-
->>>>>>> Stashed changes
 
 // Get One
 const getOneSmoothie = (req, res) => {
