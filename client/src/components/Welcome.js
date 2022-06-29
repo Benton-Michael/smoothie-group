1ccae59568018c6b9b8b4d572b80cf552bcf6fbb
Joel Latta <joellatta24@gmail.com>
2022-06-28 17:34:20 -0700

    adding toggling to login/reg

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import Register from "./Register"
-import Login from "./Login"
+import React, { useState } from "react";
+import Register from "./Register";
+import Login from "./Login";
+// import {Link} from 'react-router-dom';
 const Welcome = (props) => {
-    const {isLoggedIn, setIsLoggedIn} = props;
-    return (
-        <div className='flex justify-evenly items-center pt-5 w-full h-full'>
-            <Register setIsLoggedIn={setIsLoggedIn}/>
-            <Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+  const { isLoggedIn, setIsLoggedIn } = props;
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(false);
+  };
+  const handleUntoggle = () => {
+    setToggle(true);
+  };
+  return (
+    <div className="flex justify-evenly items-center pt-5 w-full h-full">
+      {toggle ? (
+        <div>
+          <Register setIsLoggedIn={setIsLoggedIn} />
+          <button onClick={handleToggle}>
+            Already have an account? Login here!
+          </button>
         </div>
-    );
+      ) : (
+        <div>
+          <Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+          <button onClick={handleUntoggle}>
+            Don't have an account? Register here!
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Welcome;
