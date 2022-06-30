5f88774688833e6c0b3f3eb6de22b0191b8dc4f0
Sheldon Wolff <sheldonwolff96@gmail.com>
2022-06-30 09:40:33 -0700

    p

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -9,7 +9,7 @@ module.exports = (app) => {
     "/api/users/favorite/smoothies",
     UserController.updateUsersWithFavorites
   );
-  app.get('/api/user', UserController.getOneUserCart);
+  app.get('/api/user/cart', UserController.getOneUserCart);
   app.put('/api/add/cart', UserController.addToCart);
   app.get("/api/users/favorites", UserController.getUserFavoritedSmoothies);
 };
