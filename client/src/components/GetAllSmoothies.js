9fdefbb6033479ed8b57b2b72cc45dc99ce8d4ba
Joel Latta <joellatta24@gmail.com>
2022-06-29 21:22:46 -0700

    style changes

--- client/src/components/GetAllSmoothies.js
+++ client/src/components/GetAllSmoothies.js
@@ -12,7 +12,6 @@ const GetAllSmoothies = (props) => {
   const { id } = useParams();
   const [cart, setCart] = useState([]);
 
-
   useEffect(() => {
     const userToken = Cookies.get("userToken");
     if (userToken) {
@@ -26,68 +25,84 @@ const GetAllSmoothies = (props) => {
       .get("http://localhost:5001/api/smoothies")
       .then((res) => {
         setSmoothies(res.data);
-        console.log("Setting", res.data)
+        console.log("Setting", res.data);
       })
       .catch((err) => {
         console.log("error in retreiving all smoothies", err);
       });
   }, []);
 
-  const addSmoothieToCart = e => {
+  const addSmoothieToCart = (e) => {
     e.preventDefault();
-    axios.put(`http://localhost:5001/api/add/cart/${id}`, {cart: cart}, {withCredentials: true})
+    axios
+      .put(
+        `http://localhost:5001/api/add/cart/${id}`,
+        { cart: cart },
+        { withCredentials: true }
+      )
       .then((res) => {
-        console.log(res.data.cart, user)
-        setCart([...cart, res.data.cart])
-        navigate("/details")
-        console.log(cart)
+        console.log(res.data.cart, user);
+        setCart([...cart, res.data.cart]);
+        navigate("/details");
+        console.log(cart);
       })
-      .catch(err => {
-        console.log(err)
-      })
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div className="text-center">
-        <h1 className="py-3 px-6 border-b border-gray-300 text-xl font-medium">All Smoothies</h1>
-        <div className="flex justify-center rounded-lg m-4 shadow-lg bg-white text-center grid grid-cols-2">
-          {smoothies.map((smoothie, index) => (
-          <div key={index} className="p-2 mb-4 mx-4 block rounded-lg shadow-lg bg-white text-center w-96">
+      <h1 className="py-3 px-6 border-b border-gray-300 text-xl font-medium">
+        All Smoothies
+      </h1>
+      <div className="flex justify-center rounded-lg m-4 shadow-lg bg-white text-center grid grid-cols-2">
+        {smoothies.map((smoothie, index) => (
+          <div
+            key={index}
+            className="p-2 mb-4 mx-4 block rounded-lg shadow-lg bg-white text-center w-96"
+          >
             <h5 className="text-gray-900 text-xl font-medium mb-2">
-            {smoothie.name}
+              {smoothie.name}
             </h5>
             <div>
               <div>
-              <ul className="mb-2 p-3" >
-                <li>Size: {smoothie.size}</li>
-                <li>Base: {smoothie.liquid}</li><hr/>
-                <li>
-                  <h5 className="font-medium">Fruits:</h5>
-                  {smoothie.fruits.map((fruit, i) => (
-                    <p>{fruit}</p>
-                  ))}
-                </li>
-                <hr/>
-                <li>
-                  <h5 className="font-medium">Veggies:</h5>
-                  {smoothie.veggies.map((veg, i) => (
-                    <p>{veg}</p>
-                  ))}
-                </li>
-                <hr/>
-                <li>
-                <h5 className="font-medium">Extras:</h5>
-                  {smoothie.extras.map((ex, i) => (
-                    <p>{ex}</p>
-                  ))}
+                <ul className="mb-2 p-3">
+                  <li>Size: {smoothie.size}</li>
+                  <li>Base: {smoothie.liquid}</li>
+                  <hr />
+                  <li>
+                    <h5 className="font-medium">Fruits:</h5>
+                    {smoothie.fruits.map((fruit, i) => (
+                      <p>{fruit}</p>
+                    ))}
                   </li>
                   <hr />
-              </ul>
-            </div>
-            <button onClick={addSmoothieToCart}>Add To Cart</button>
+                  <li>
+                    <h5 className="font-medium">Veggies:</h5>
+                    {smoothie.veggies.map((veg, i) => (
+                      <p>{veg}</p>
+                    ))}
+                  </li>
+                  <hr />
+                  <li>
+                    <h5 className="font-medium">Extras:</h5>
+                    {smoothie.extras.map((ex, i) => (
+                      <p>{ex}</p>
+                    ))}
+                  </li>
+                  <hr />
+                </ul>
+              </div>
+              <button
+                onClick={addSmoothieToCart}
+                className="dark:bg-green-800 hover:bg-slate-400 text-white py-1 px-1 rounded"
+              >
+                Add To Cart
+              </button>
             </div>
           </div>
-          ))}
-        </div>
+        ))}
+      </div>
     </div>
   );
 };
